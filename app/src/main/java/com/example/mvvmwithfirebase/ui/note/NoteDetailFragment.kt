c4b9d9f0090a44c8e8834ac5fab5b5d5751e1643
Hp <saidisxoq@gmail.com>
2022-11-23 15:26:37 +0300

    where equal to completed 23-lesson

--- app/src/main/java/com/example/mvvmwithfirebase/ui/note/NoteDetailFragment.kt
+++ app/src/main/java/com/example/mvvmwithfirebase/ui/note/NoteDetailFragment.kt
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.mvvmwithfirebase.R
 import com.example.mvvmwithfirebase.data.model.Note
 import com.example.mvvmwithfirebase.databinding.FragmentNoteDetailBinding
+import com.example.mvvmwithfirebase.ui.auth.AuthViewModel
 import com.example.mvvmwithfirebase.util.*
 import com.google.android.material.button.MaterialButton
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +24,7 @@ class NoteDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentNoteDetailBinding
     private val viewModel: NoteViewModel by viewModels()
+    private val authViewModel: AuthViewModel by viewModels()
     private var objNote: Note? = null
     private var tagsList: MutableList<String> = arrayListOf()
 
@@ -238,6 +240,6 @@ class NoteDetailFragment : Fragment() {
             description = binding.description.text.toString(),
             tags = tagsList,
             date = Date()
-        )
+        ).apply { authViewModel.getSession { this.user_id = it?.id ?: "" } }
     }
 }
